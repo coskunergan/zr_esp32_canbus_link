3c9d65c00598b1370a471a9ff58aa901fc8d547c
coskunergan <coskunergan@gmail.com>
2025-10-18 22:14:24 +0300

    V1.0.1
    -Modbus working.

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,7 @@ use static_cell::StaticCell;
 use zephyr::device::gpio::GpioPin;
 
 use portable_atomic::{AtomicU16, Ordering};
+//use core::{sync::atomic::AtomicU16, sync::atomic::Ordering};
 
 // use canbus::CanBus;
 use display_io::Display;
@@ -109,25 +110,20 @@ extern "C" fn rust_main() {
 
     log::info!("Restart!!!\r\n");
 
+    RED_LED_PIN.init(Pin::new(
+        zephyr::devicetree::labels::red_led::get_instance().expect("my_red_led not found!"),
+    ));
+
     let mut local_reg = 0x123;
 
     // let mut can_fd = CanBus::new("canbus0\0");
     // can_fd.set_data_callback(receive_callback);
 
     let modbus = ModbusSlave::new("modbus0\0");
-    // let modbus_vcp = ModbusSlave::new("modbus1\0");
 
     modbus.mb_add_holding_reg(COUNTER.as_ptr(), 0);
     modbus.mb_add_holding_reg(REGISTER.as_ptr(), 1);
     modbus.mb_add_holding_reg(&mut local_reg, 2);
-
-    // modbus_vcp.mb_add_holding_reg(COUNTER.as_ptr(), 0);
-    // modbus_vcp.mb_add_holding_reg(REGISTER.as_ptr(), 1);
-    // modbus_vcp.mb_add_holding_reg(&mut local_reg, 2);
-
-    RED_LED_PIN.init(Pin::new(
-        zephyr::devicetree::labels::red_led::get_instance().expect("my_red_led not found!"),
-    ));
 
     let executor = EXECUTOR_MAIN.init(Executor::new());
     executor.run(|spawner| {
